068cf5111e46233b92b671c3a929dbf889e41ffa
youngkyunge <162668890+youngkyunge@users.noreply.github.com>
2024-04-19 14:26:57 +0900

    Update starter.js

--- src/starter.js
+++ src/starter.js
@@ -6,8 +6,8 @@ import "./viz.css";
 // svg
 
 const svg = d3.select("#svg-container").append("svg").attr("id", "svg");
-const width = parseInt(d3.select("#svg-container").style("width"));
-const height = parseInt(d3.select("#svg-container").style("height"));
+let width = parseInt(d3.select("#svg-container").style("width"));
+let height = parseInt(d3.select("#svg-container").style("height"));
 
 console.log(width + "," + height);
 const margin = { top: 65, right: 50, bottom: 65, left: 50 };
@@ -56,12 +56,31 @@ let data = [];
 let selectedPlayer;
 
 let radiusAxis, angleAxis, labels;
+let path;
+let players;
+let selectedName = "H. Son";
 
 d3.json("data/fifa23_maleplayers.json").then((raw_data) => {
   data = raw_data.filter((d) => d.overall > 85);
-  selectedPlayer = data.filter((d) => d.short_name === "H. Son")[0];
 
-  // console.log(selectedPlayer);
+  players = [...new Set(data.map((d) => d.short_name))];
+
+  selectedPlayer = data.filter((d) => d.short_name == selectedName[0]);
+
+  const dropdown = document.getElementById("options");
+
+  players.map((d) => {
+    const option = document.createElement("option");
+    option.value = d;
+    option.innerHTML = d;
+    option.selected = d === selectedname ? true : false;
+    dropdown.appendChild(option);
+  });
+
+  dropdown.addEventListener("change", function () {
+    selectedName = dropdown.value;
+    updatePlayer();
+  });
 
   //axis
 
@@ -107,6 +126,8 @@ d3.json("data/fifa23_maleplayers.json").then((raw_data) => {
     .attr("stroke", pointColor)
     .attr("stroke-width", 1.3)
     .style("fill-opacity", 0.1);
+
+  d3.select("#player-name").text(selectedPlayer.long_name);
 });
 
 // function
@@ -116,6 +137,48 @@ const getXPos = (dist, index) => {
 };
 
 const getYPos = (dist, index) => {
-  //radius * cos (theta)
+  //radius * sin (theta)
   return radiusScale(dist) * Math.sin(angleScale(index) - Math.PI / 2);
 };
+
+// Update
+const updatePlayer = () => {
+  selectedPlayer = data.filter((d) => d.shortname == selectedName)[0];
+  console.log(selectedPlayer);
+
+  radarLine.radius((d) => radiusScale(selectedPlayer[d]));
+
+  path.transition().duration(600).attr("d", radarLine);
+  d3.select("#player-name").text(selectedPlayer.long_name);
+};
+
+// Resize
+window.addEventListener("resize", () => {
+  width = parseInt(d3.select("#svg-container").style(width));
+  height = parseInt(d3.select("#svg-container").style(height));
+
+  g.attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  //scale
+  minLen = d3.min([height / 2 - margin.top, width / 2 - margin.right]);
+
+  radiusScale.range([0, minLen]);
+
+  // axis
+
+  radiusAxis.attr("r", (d) => radiusScale(d));
+
+  angleAxis
+    .attr("x2", (d, i) => getXPos(100, i))
+    .attr("y2", (d, i) => getYPos(100, i));
+
+  // path
+  radarLine.radius((d) => radiusScale(selectedPlayer[d]));
+
+  path.attr("d", radarLine);
+
+  // label
+  labels
+    .attr("x", (d, i) => getXPos(116, i))
+    .attr("y", (d, i) => getYPos(116, i));
+});
